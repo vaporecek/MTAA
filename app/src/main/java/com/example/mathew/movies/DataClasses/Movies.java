abc96ce27beaa91eb68034d60dfb677234ffa727
cagankarol <cagan.karol@gmail.com>
2016-04-02 21:52:27 +0200

    Upratanie a oddelenie backendu od inych klassov

---
+++ app/src/main/java/com/example/mathew/movies/DataClasses/Movies.java
@@ -0,0 +1,19 @@
+package com.example.mathew.movies.DataClasses;
+
+import java.util.Date;
+
+/**
+ * Created by Karol on 02.04.2016.
+ */
+public class Movies {
+    int country;
+    String description;
+    int gender;
+    String picture;
+    Date play_time;
+    int rating;
+    String screenplay;
+    String title;
+    String to_watch;
+
+}
